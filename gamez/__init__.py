df32314801fb5a5936794d5d8d8d0ecd333d744f
Rob <rtruxal@gmail.com>
2016-07-01 00:34:38 -0700

    turned start_game into pkg lvl function
    also made game_funcs at pkg level

--- gamez/__init__.py
+++ gamez/__init__.py
@@ -1,21 +1,5 @@
+from play.run import start_game
+from utils.game_funcs import *
+
+
 __all__ = ['__doc__', 'character', 'combat', 'game_funcs', 'powers', 'prompts', 'items']
-
-# class __init__:
-#
-#     def won_game(self, **char_names):
-#         """
-#         Here we'll fux with the winning parameters
-#
-#         Nothing will be returned, but if a check is done and win = true, the gamez will end and show end-gamez stats
-#         """
-#     def gen_map(self, **map_params):
-#         """
-#
-#         same as above but w map
-#
-#
-#         :param map_params:
-#         :return:
-#         """
-
-
